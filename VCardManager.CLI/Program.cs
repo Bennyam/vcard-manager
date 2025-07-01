d57d8aac64323f05614bb92bf0be6fc46bebb065
Bennyam <38783044+Bennyam@users.noreply.github.com>
2025-07-01 17:55:39 +0200

    Final working version VCard Manager CLI, all tests green

--- VCardManager.CLI/Program.cs
+++ VCardManager.CLI/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using VCardManager.Core;
+
+namespace VCardManager.CLI
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      IConsole console = new SystemConsole();
+      IFileStore fileStore = new FileSystemStore();
+      IVCardConverter converter = new VCardConverter();
+      string filePath = "data/contacts.vcf";
+
+      var service = new ContactService(fileStore, converter, filePath);
+      var menu = new Menu(console, service);
+
+      menu.Run();
+    }
+  }
+}
